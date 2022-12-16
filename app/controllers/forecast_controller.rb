8b39016b36bf6ceb138528d1dd0f84fb8501d7a0
Carlos Feliciano-Barba <carlos@felicianobarba.com>
2022-12-16 02:33:24 -0400

    Cache forecast response for 30 min based on the params used
    - Add test to ensure that caching takes place

--- app/controllers/forecast_controller.rb
+++ app/controllers/forecast_controller.rb
@@ -1,11 +1,21 @@
 class ForecastController < ApplicationController
   def index
-    # TODO Add redis caching for 30min using params as key
     begin
+      $redis = Redis.new
+
+      # Return the cached result if it exists
+      if $redis.exists?(params_key)
+        cached = JSON.parse($redis.get(params_key))
+        return render json: { forecast: cached }
+      end
+
       @forecast = WeatherAPI.fetch_forecast(
         forecast_params.fetch(:location, nil),
         is_zip?,
         forecast_params.fetch(:unit, 'imperial'))
+
+      # Store the fetched forecast for 30 min
+      $redis.set(params_key, @forecast.to_json, ex: 1800)
 
       return render json: { forecast: @forecast }
     rescue => e
@@ -17,6 +27,11 @@ class ForecastController < ApplicationController
 
   def forecast_params
     params.require(:forecast).permit(:location, :unit);
+  end
+
+  # Creates a unique key based on the params on the request
+  def params_key
+    forecast_params.to_json
   end
 
   # If there's only numbers in the location assume it's a zip code
--- spec/requests/forecast_spec.rb
+++ spec/requests/forecast_spec.rb
@@ -44,5 +44,27 @@ RSpec.describe 'Forecasts', type: :request do
         expect(forecast.temperature_unit).to eq('metric')
       end
     end
+
+    context 'when the response is cached' do
+      before(:each) do
+        # Ensure that the key is not in Redis when we start testing
+        $redis = Redis.new
+        $redis.del({"location":"90210","unit":"metric"}.to_json)
+      end
+
+      it 'only calls the WeatherAPI once' do
+        allow(WeatherAPI).to(receive(:fetch_forecast)).and_call_original
+
+        get '/forecast', params: { forecast: { location: '90210', unit: 'metric' }}
+        get '/forecast', params: { forecast: { location: '90210', unit: 'metric' }}
+
+        expect(WeatherAPI).to have_received(:fetch_forecast).once
+
+        expect(response).to have_http_status(:success)
+        expect(forecast.city.name).to eq("Beverly Hills")
+        expect(forecast.weather_conditions.length).to eq(40)
+        expect(forecast.temperature_unit).to eq('metric')
+      end
+    end
   end
 end
